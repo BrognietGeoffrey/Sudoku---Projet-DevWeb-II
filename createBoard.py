c131f22bf8401a347d901be1bfb9dbebd11904c6
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2020-10-24 16:27:11 +0200

    Add files via upload

--- createBoard.py
+++ createBoard.py
@@ -3,7 +3,7 @@ import random as r
 board = [[1,2,3,4,5,6,7,8,9] for i in range(9)]
 
 def createBoard():
-	board2 = board[:]
+
 	for i in range(9):
 		r.shuffle(board[i])
 
@@ -11,33 +11,26 @@ def verifColonne():
 	isok = False
 	for colonne in range(9):
 		numbers = []
-		for ligne in board:
-			if ligne[colonne] in numbers:
-				numbers.append(ligne[colonne])
+		for ligne in range(9):
+			if board[ligne][colonne] not in numbers:
+				numbers.append(board[ligne][colonne])
 		if not uniqueNumbers(numbers):
 			return False
 		else:
 			isok = True
 	return isok
-"""
-def verifLigne():
-	for ligne in board:
-		if not uniqueNumbers(ligne):
-			return False
-	return True
-"""	
 	
 def verifDiagonale():
 	numbers = []
 	numbers2 = []
 	for i in range(9):
 		numbers.append(board[i][i])
-		numbers.append(board[-i][-i])
+		numbers2.append(board[-i][-i])
 	return uniqueNumbers(numbers) and uniqueNumbers(numbers2)
 		
 
 def uniqueNumbers(numbers):
-	return numbers.sort() == [1,2,3,4,5,6,7,8,9]
+	return sorted(numbers) == [1,2,3,4,5,6,7,8,9]
 
 def printBoard():
 	for i in range(9):
