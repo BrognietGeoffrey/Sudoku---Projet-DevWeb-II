568635afed5e56b3d69a33b80d400c8fb1cbe5e7
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2020-10-24 14:26:48 +0200

    Add files via upload

---
+++ createBoard.py
@@ -0,0 +1,48 @@
+import random
+
+board = [[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9],
+		[1,2,3,4,5,6,7,8,9]]
+
+def createBoard(board):
+	pass
+	
+	
+
+def verifColonne():
+	isok = False
+	for colonne in range(9):
+		numbers = []
+		for ligne in board:
+			if ligne[colonne] in numbers:
+				numbers.append(ligne[colonne])
+		if not uniqueNumbers(numbers):
+			return False
+		else:
+			isok = True
+	return isok
+"""
+def verifLigne():
+	for ligne in board:
+		if not uniqueNumbers(ligne):
+			return False
+	return True
+"""	
+	
+def verifDiagonale():
+	numbers = []
+	numbers2 = []
+	for i in range(9):
+		numbers.append(board[i][i])
+		numbers.append(board[-i][-i])
+	return uniqueNumbers(numbers) and uniqueNumbers(numbers2)
+		
+
+def uniqueNumbers(numbers):
+	return numbers.sort() == [1,2,3,4,5,6,7,8,9]
