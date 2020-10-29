56f54957a45580f6d9e8ae1ea989ed2d3f6ee563
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2020-10-29 21:21:48 +0100

    Add files via upload

--- createBoard.py
+++ createBoard.py
@@ -1,45 +1,41 @@
 from random import sample
 
 board = []
-carre = 3
-taille = carre**2
-compteur_taille_carre = range(carre)
-
+taille_carre = 3
+taille = taille_carre**2
+compteur_taille_carre = range(taille_carre)
 
 def create_board():
-	rows, cols = [], []
-	for r in shuffle(compteur_taille_carre):
-		for g in shuffle(compteur_taille_carre):
-			rows.append(g * carre + r)
+	lignes, colonnes = [], []
+	for i in shuffle(compteur_taille_carre):
+		for j in shuffle(compteur_taille_carre):
+			colonnes.append(j * taille_carre + i)
 
-	for c in shuffle(compteur_taille_carre):
-		for g in shuffle(compteur_taille_carre):
-			cols.append(g * carre + c)
+	for i in shuffle(compteur_taille_carre):
+		for j in shuffle(compteur_taille_carre):
+			lignes.append(j * taille_carre + i)
 
 	nums = shuffle(range(1, taille + 1))
 
 	# produce board using randomized carreline pattern
-	for r in rows:
+	for i in lignes:
 		temp = []
-		for c in cols:
-			temp.append(nums[pattern(r, c)])
+		for j in colonnes:
+			temp.append(nums[pattern(i, j)])
 		board.append(temp)
 
-
-def pattern(r, c):
+def pattern(ligne, colonne):
 	# pattern for a carreline valid solution
-	return (carre * (r % carre) + r // carre + c) % taille
+	return (taille_carre * (ligne % taille_carre) + ligne // taille_carre + colonne) % taille
 
-
-def shuffle(s):
-	# randomize rows, columns and numbers (of valid carre pattern)
-	return sample(s, len(s))
-
+def shuffle(tableau):
+	# Mélange les lignes et les colonnes
+	return sample(tableau, len(tableau))
 
 def print_board():
 	for i in range(taille):
-		if i % 3 == 0 or i==0:
-			print(" =" * (taille * 2 + 2))
+		if i%3==0 or i==0:
+			print(" =" *(taille*2+2))
 		print("||", end="")
 		for j,x in enumerate(board[i]):
 			if j%3==0 and j!=0:
@@ -48,7 +44,6 @@ def print_board():
 		print("|")
 		if i==taille-1:
 			print(" ="*(taille*2+2))
-
 
 create_board()
 print_board()
