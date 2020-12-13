738dcc2144970c3d1687cca921e558edfeedf1dd
BrognietGeoffrey <58220770+BrognietGeoffrey@users.noreply.github.com>
2020-12-13 17:56:42 +0100

    Gui to be continued

--- Code/GUI/sudokugui.py
+++ Code/GUI/sudokugui.py
@@ -2,6 +2,14 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.accordion import Accordion, AccordionItem
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+
+from kivy.uix.label import Label
+from kivy.uix.accordion import Accordion, AccordionItem
 
 
 class WindowManager(ScreenManager):
@@ -35,10 +43,31 @@ class RegisterWindow(BoxLayout, Screen):
 
 
 class PlayerWindow(BoxLayout, Screen):
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        btn2 = Button(
+            text='New Game',
+            size_hint=(.5, .25),
+            pos_hint={'left': 0, 'top': 1}
+        )
+        """btn2.bind(on_press=self.change_screen_to_game())
+        self.add_widget(btn2)"""
+
+
+
+        for x in range(5):
+            item = AccordionItem()
+            item.add_widget(Label(text='Very big content\n' * 10))
+            self.add_widget(item)
 
 
 class GameWindow(BoxLayout, Screen):
+    pass
+
+
+class SaveGame():
     pass
 
 
