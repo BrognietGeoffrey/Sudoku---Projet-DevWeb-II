edfd7bf75f390afb8fb4d50861bb1033a8b5700a
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2020-12-14 23:22:26 +0100

    Add files via upload

--- Code/gui.py
+++ Code/gui.py
@@ -14,22 +14,19 @@ class Window(QWidget):
         self.resize(512, 512)
         self.setWindowTitle("Sudoku ")
 
-        # Create a QHBoxLayout instance
     def paintEvent(self, event):
         painter = QPainter(self)
         size = 40
 
-        for i in range(9):
-            for j in range(9):
-                painter.setPen(Qt.black)
-                painter.drawRect(80+(j*size), 10+(i*size), size, size)
+        for i in range(10):
+            if i < 9:
+                for j in range(9):
+                    painter.setPen(Qt.black)
+                    painter.drawRect(80+(j*size), 10+(i*size), size, size)
+            if i % 3 == 0:
+                painter.setPen(Qt.cyan)
+                painter.drawLine(80, 10+(i*size), 11 * size, 10+(i*size))
 
-        # Set the layout on the application's window
-        painter.setPen(Qt.cyan)
-        painter.drawLine(80, 10, 11 * size, 10)
-        painter.drawLine(80, 130, 11 * size, 130)
-        painter.drawLine(80, 250, 11 * size, 250)
-        painter.drawLine(80, 370, 11 * size, 370)
         print(self.children())
 
 
