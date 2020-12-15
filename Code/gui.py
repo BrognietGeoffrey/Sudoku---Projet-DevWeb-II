2dcffc4cf49152ee1ce7376ae1477afbede47704
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2020-12-15 14:49:21 +0100

    Grille fonctionnelle et séparation par carré

--- Code/gui.py
+++ Code/gui.py
@@ -17,15 +17,24 @@ class Window(QWidget):
     def paintEvent(self, event):
         painter = QPainter(self)
         size = 40
+        shift = 70
 
         for i in range(10):
             if i < 9:
-                for j in range(9):
-                    painter.setPen(Qt.black)
-                    painter.drawRect(80+(j*size), 10+(i*size), size, size)
+                for j in range(10):
+                    if j < 9:
+                        # Draws the squares of the Sudoku
+                        painter.setPen(Qt.black)
+                        painter.drawRect(80 + (j * size), shift + (i * size), size, size)
+                    if j % 3 == 0:
+                        # Trace vertical lines
+                        painter.setPen(Qt.cyan)
+                        painter.drawLine(10 + shift + (j * size), shift, 10 + shift + (j * size), size * 9 + shift)
             if i % 3 == 0:
+                # Trace horizontal lines
                 painter.setPen(Qt.cyan)
-                painter.drawLine(80, 10+(i*size), 11 * size, 10+(i*size))
+                painter.drawLine(80, shift + (i * size), 11 * size, shift + (i * size))
+
 
         print(self.children())
 
