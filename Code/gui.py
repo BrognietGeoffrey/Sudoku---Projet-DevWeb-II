5bd4da890191958ea2d86a76630061f954278e22
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2020-12-15 22:27:35 +0100

    add timer

--- Code/gui.py
+++ Code/gui.py
@@ -5,14 +5,48 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from datetime import timedelta
 
-class Window(QWidget):
-
+class MyMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
 
         super().__init__()
         self.resize(512, 512)
         self.setWindowTitle("Sudoku ")
+
+        self.my_qtimer = QtCore.QTimer(self)
+        self.game_time = 0
+        self.widget_counter_int = 0
+
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
+        vbox = QtWidgets.QVBoxLayout()
+        central_widget.setLayout(vbox)
+
+        self.pages_qsw = QtWidgets.QStackedWidget()
+        vbox.addWidget(self.pages_qsw)
+        self.time_passed = QtWidgets.QLabel()
+        vbox.addWidget(self.time_passed)
+
+        self.timer_start()
+        self.update_gui()
+
+    def timer_start(self):
+        # Starts the timer
+        self.my_qtimer.timeout.connect(self.timer_timeout)
+        self.my_qtimer.start(1000)
+        self.update_gui()
+
+    def timer_timeout(self):
+        # Counts the time
+        self.game_time += 1
+        self.update_gui()
+
+    def update_gui(self):
+        # Update the time
+        self.time_passed.setText(str(timedelta(seconds=self.game_time)))
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -25,7 +59,7 @@ class Window(QWidget):
                 # Draws the squares of the Sudoku
                 painter.setPen(Qt.gray)
                 painter.drawRect(80 + (j * size), shift + (i * size), size, size)
-                        
+
         for i in [0,3,6,9]:
             # Trace vertical lines
             painter.setPen(color)
@@ -35,8 +69,7 @@ class Window(QWidget):
 
 
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
-    window = Window()
+    window = MyMainWindow()
     window.show()
     sys.exit(app.exec_())
