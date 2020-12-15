f714ddd434650084b061986cc0649aea8145bc75
jeanmtang <32909559+jeanmtang@users.noreply.github.com>
2020-12-15 16:18:13 +0100

    Optimisation

--- Code/gui.py
+++ Code/gui.py
@@ -1,8 +1,8 @@
 import sys
+from Sudoku import create_board
 
-
-from PyQt5.QtGui     import *
-from PyQt5.QtCore    import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 
@@ -18,25 +18,20 @@ class Window(QWidget):
         painter = QPainter(self)
         size = 40
         shift = 70
+        color = Qt.black
 
-        for i in range(10):
-            if i < 9:
-                for j in range(10):
-                    if j < 9:
-                        # Draws the squares of the Sudoku
-                        painter.setPen(Qt.black)
-                        painter.drawRect(80 + (j * size), shift + (i * size), size, size)
-                    if j % 3 == 0:
-                        # Trace vertical lines
-                        painter.setPen(Qt.cyan)
-                        painter.drawLine(10 + shift + (j * size), shift, 10 + shift + (j * size), size * 9 + shift)
-            if i % 3 == 0:
-                # Trace horizontal lines
-                painter.setPen(Qt.cyan)
-                painter.drawLine(80, shift + (i * size), 11 * size, shift + (i * size))
-
-
-        print(self.children())
+        for i in range(9):
+            for j in range(9):
+                # Draws the squares of the Sudoku
+                painter.setPen(Qt.gray)
+                painter.drawRect(80 + (j * size), shift + (i * size), size, size)
+                        
+        for i in [0,3,6,9]:
+            # Trace vertical lines
+            painter.setPen(color)
+            painter.drawLine(10 + shift + (i * size), shift, 10 + shift + (i * size), size * 9 + shift)
+            # Trace horizontal lines
+            painter.drawLine(80, shift + (i * size), 11 * size, shift + (i * size))
 
 
 if __name__ == "__main__":
